6c731786beeb9d4c51f645218e985a349e955e0a
Miriam-Villalta <mvb_8@hotmail.com>
2021-09-14 14:02:28 +0200

    add logo

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@ function getDogImage() {
   fetch("https://dog.ceo/api/breeds/image/random")
     .then((response) => response.json())
     .then((data) => {
-      const img = document.querySelector("img");
+      const img = document.querySelector(".js-dog");
       img.src = data.message;
-      img.alt = "Un chihuahua";
+      img.alt = "Un perro";
     });
 }
-const btn = document.querySelector(".js-dog");
+const btn = document.querySelector(".js-dogbtn");
 btn.addEventListener("click", getDogImage);
